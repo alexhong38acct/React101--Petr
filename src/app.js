fe12fe4a270f5eaebb93fc0608953e3b8ac32122
Alex Hong <alexh@lawpromo.com>
2017-11-16 14:49:55 -0800

    lesson3

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const Headline = () => {
+	return <h1 className="title">Welcome to the React World.</h1>
+}
+
+const Greeting = (props) => {
+	const {name, age} = props;				// destructuring props
+	return <p>You will love it {name} ({age})!</p>
+	// return <p>You will love it {props.name} ({props.age})!</p>
+}
 
 // 	Stateless Component : App
 export const App = () => {
 	return (
 		<div>
-			<h1 className="title">Hello World</h1>
-			<p>Testing rendering with JSX.</p>		
+			<Headline />
+			<Greeting name="Petr" age={25} />
 		</div>
 	)
 }
+
+
+Greeting.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number.isRequired
+}
+
+
+
 
 // export default App;

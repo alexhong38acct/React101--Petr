a903ed0e548539bd612f0bbd04394423c9c54b35
Alex Hong <alexh@lawpromo.com>
2017-11-16 16:09:32 -0800

    lesson4

--- src/app.js
+++ src/app.js
@@ -1,34 +1,102 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Headline = () => {
-	return <h1 className="title">Welcome to the React World.</h1>
+export class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			buyItems: ['milk', 'bread', 'fruit']
+		}
+	}
+
+	render() {
+		// 	Destructuring Way
+		const {buyItems} = this.state;
+		return (
+			<div>
+				<h1>Shopping List</h1>
+				{
+					// 	this.state.buyItems.map( item => {
+					
+					// 	Destructured Way
+					buyItems.map( item => {
+						return <p key={item}>{item}</p>
+					})
+				}
+			</div>
+		)
+	}
 }
 
-const Greeting = (props) => {
-	const {name, age} = props;				// destructuring props
-	return <p>You will love it {name} ({age})!</p>
-	// return <p>You will love it {props.name} ({props.age})!</p>
-}
-
-// 	Stateless Component : App
-export const App = () => {
-	return (
-		<div>
-			<Headline />
-			<Greeting name="Petr" age={25} />
-		</div>
-	)
-}
-
-
-Greeting.propTypes = {
-	name: PropTypes.string,
-	age: PropTypes.number.isRequired
-}
 
 
 
+
+
+
+// const Headline = () => {
+// 	return <h1 className="title">Welcome to the React World.</h1>
+// }
+// const Greeting = (props) => {
+// 	//  Normal way
+// 	// 	return <p>You will love it {props.name} ({props.age})!</p>
+	
+// 	// 	Destructuring props way
+// 	const {name, age} = props;
+// 	return <p>You will love it {name} ({age})!</p>
+// }
+// Greeting.propTypes = {
+// 	name: PropTypes.string,
+// 	age: PropTypes.number.isRequired
+// }
+
+// 	Stateless Component Way
+//  const App = () => {
+// 	return (
+// 		<div>
+// 			<Headline />
+// 			<Greeting name="Petr" age={25} />
+// 		</div>
+// 	)
+// }
+
+//  Class Component Way
+// export class App extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<Headline />
+// 				<Greeting name="Petsr" age={25} />
+// 			</div>
+// 		)
+// 	}
+// }
 
 // export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
